40143234a7b127755dd0382710196a1e1b427763
Kevin harrington <mad.hephaestus@gmail.com>
2025-01-01 10:51:17 -0500

    Selection box is Working using the ray-cast Hull intersetcion trick!

--- src/main/java/com/commonwealthrobotics/SelectionBox.java
+++ src/main/java/com/commonwealthrobotics/SelectionBox.java
@@ -1,6 +1,7 @@
 package com.commonwealthrobotics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,9 @@ import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 import eu.mihosoft.vrl.v3d.Bounds;
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.Cube;
+import eu.mihosoft.vrl.v3d.Transform;
 import eu.mihosoft.vrl.v3d.Vector3d;
+import eu.mihosoft.vrl.v3d.ext.quickhull3d.HullUtil;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
@@ -44,8 +47,10 @@ public class SelectionBox {
 	private Affine wpPickPlacement = new Affine();
 	private Affine dottedLine = new Affine();
 
-	private ArrayList<Rectangle> show = new ArrayList<>();
+	//private ArrayList<Rectangle> show = new ArrayList<>();
 	private EventHandler<? super MouseEvent> eventFilter;
+	private HashMap<CSG, Bounds> cache = new HashMap<>();
+	private CSG selection;
 
 	public SelectionBox(SelectionSession session, AnchorPane view3d, BowlerStudio3dEngine engine, ActiveProject ap) {
 		this.session = session;
@@ -61,7 +66,7 @@ public class SelectionBox {
 		getSelectionPlane().setCullFace(CullFace.BACK);
 		getSelectionPlane().setMaterial(material);
 		getSelectionPlane().setOpacity(0.0025);
-		getSelectionPlane().getTransforms().addAll(wpPickPlacement);
+		getSelectionPlane().getTransforms().addAll(dottedLine);
 
 		getSelectionPlane().setViewOrder(-2);
 		getSelectionPlane().addEventFilter(MouseEvent.MOUSE_DRAGGED, event -> {
@@ -97,6 +102,8 @@ public class SelectionBox {
 	public void dragged(MouseEvent event) {
 		if(!event.isPrimaryButtonDown())
 			return;
+		if(!(event.getSource()==engine.getSubScene()||event.getSource()==getSelectionPlane()))
+			return;
 		if (!engine.contains(rect)) {
 			engine.addUserNode(rect);
 			active = false;
@@ -105,6 +112,9 @@ public class SelectionBox {
 		if (source == engine.getSubScene())
 			return;
 		if (!active) {
+			TransformNR cf = getCameraFrame();
+//			ap.get().setWorkplane(cf);
+//			workplane.placeWorkplaneVisualization();
 			xStart = event.getX();
 			yStart = event.getY();
 			xLeft = xStart;
@@ -120,12 +130,36 @@ public class SelectionBox {
 			rect.setWidth(0);
 			rect.setHeight(0);
 			rect.setViewOrder(-2);
+
+			//show.clear();
+			cache.clear();
+			new Thread(()->{
+				ArrayList<CSG> visable = session.getAllVisable();
+				for (CSG c : visable)
+					session.getBounds(c, cf, cache);
+//				for(CSG key:cache.keySet()) {
+//					Bounds b= key.getBounds();
+//					Rectangle r = new Rectangle();
+//					r.setX(b.getMinX());
+//					r.setY(b.getMinY());
+//					r.setWidth(b.getTotalX());
+//					r.setHeight(b.getTotalY());
+//					r.getTransforms().addAll(wpPickPlacement);
+//					r.setVisible(true);
+//					r.setFill(Color.TRANSPARENT);
+//					r.setStroke(Color.BLACK);
+//					r.setStrokeWidth(1);
+//					r.getStrokeDashArray().addAll(2.0, 2.0);
+//					r.setMouseTransparent(true);
+//					engine.addUserNode(r);
+//					show.add(r);
+//				}
+				
+			}).start();
 			active = true;
+			
 		}
-		for (Rectangle r : show) {
-			view3d.getChildren().remove(r);
-		}
-		show.clear();
+		
 		// Auto-generated method stub
 		double width = Math.abs(event.getX() - xStart);
 		double height = Math.abs(event.getY() - yStart);
@@ -148,7 +182,9 @@ public class SelectionBox {
 //		rect.setVisible(false);
 //		view3d.getChildren().remove(rect);	
 		engine.removeUserNode(rect);
-		//engine.removeUserNode(selectionPlane);
+//		for (Rectangle r : show) {
+//			engine.removeUserNode(r);
+//		}
 		new Thread(() -> {
 			double width = Math.abs(event.getX() - xStart);
 			double height = Math.abs(event.getY() - yStart);
@@ -162,22 +198,35 @@ public class SelectionBox {
 			active = false;
 //			yTop=yTop-view3d.getHeight()/2;
 //			xLeft=xLeft-(view3d.getWidth()/2);
-			TransformNR cf = getCameraFrame();
+//			ap.get().setWorkplane(cf);
+//			workplane.placeWorkplaneVisualization();
 
-			ArrayList<CSG> visable = session.getAllVisable();
-			HashMap<CSG, Bounds> cache = new HashMap<>();
-			for (CSG c : visable)
-				session.getBounds(c, cf, cache);
 			Bounds sel = new Bounds(new Vector3d(xLeft, yTop, 0), new Vector3d(xLeft + width, yTop + width));
-			List<String> overlapping = checkOverlap(sel, cache);
+			Transform boxFrame = TransformFactory.nrToCSG(getBoxFrame());
+//			if(selection!=null)
+//				BowlerStudio.runLater(()-> engine.removeObject(selection));
+			selection = HullUtil.hull(Arrays.asList(
+					new Vector3d(sel.getMaxX(),sel.getMaxY(),0).transform(boxFrame),
+					new Vector3d(sel.getMaxX(),sel.getMinY(),0).transform(boxFrame),
+					new Vector3d(sel.getMinX(),sel.getMaxY(),0).transform(boxFrame),
+					new Vector3d(sel.getMinX(),sel.getMinY(),0).transform(boxFrame),
+					new Vector3d(0,0,0).transform(TransformFactory.nrToCSG(getCameraFrame()))
+					));
+			
+			if(selection==null)
+				throw new RuntimeException("Selection can not be null");
+			//selection.setManipulator(dottedLine);
+			selection.setColor(new Color(0.25, 0.25, 0, 0.25));
+			//engine.addCsg(selection, null);
+			List<String> overlapping = checkOverlap(selection, cache);
 			session.selectAll(overlapping);
 		}).start();
 	}
 
 	private TransformNR getCameraFrame() {
 		VirtualCameraMobileBase camera = engine.getFlyingCamera();
-		double zoom = camera.getZoomDepth() /2;
-		TransformNR cf = engine.getFlyingCamera().getCamerFrame().times(new TransformNR(0, 0, 500));
+		double zoom = camera.getZoomDepth() ;
+		TransformNR cf = engine.getFlyingCamera().getCamerFrame().times(new TransformNR(0, 0, zoom));
 		return cf;
 	}
 	private TransformNR getBoxFrame() {
@@ -186,27 +235,32 @@ public class SelectionBox {
 		TransformNR cf = engine.getFlyingCamera().getCamerFrame().times(new TransformNR(0, 0, 500));
 		return cf;
 	}
+	/**
+	 * A test to see if 2 CSG's are touching. The fast-return is a bounding box
+	 * check If bounding boxes overlap, then an intersection is performed and the
+	 * existance of an interscting object is returned
+	 * 
+	 * @param incoming
+	 * @return
+	 */
+	public boolean touching(CSG selection2,Bounds incoming) {
+		// Fast bounding box overlap check, quick fail if not intersecting
+		// bounding boxes
+		if (selection2.getMaxX() > incoming.getMinX() && selection2.getMinX() < incoming.getMaxX()
+				&& selection2.getMaxY() > incoming.getMinY() && selection2.getMinY() < incoming.getMaxY()
+				&& selection2.getMaxZ() > incoming.getMinZ() && selection2.getMinZ() < incoming.getMaxZ()) {
+			return true;
+		}
+		return false;
+	}
 
-	public List<String> checkOverlap(Bounds sel, Map<CSG, Bounds> cache) {
+	public List<String> checkOverlap(CSG selection2, Map<CSG, Bounds> cache) {
 		List<String> overlapping = new ArrayList<>();
 
 		for (CSG key : cache.keySet()) {
 			Bounds bounds = cache.get(key);
-
-			// Get coordinates of selection box
-			double selMaxX = sel.getMaxX();
-			double selMaxY = sel.getMaxY();
-			double selMinX = sel.getMinX();
-			double selMinY = sel.getMinY();
-
-			// Get coordinates of current box
-			double boxMaxX = bounds.getMaxX();
-			double boxMaxY = bounds.getMaxY();
-			double boxMinX = bounds.getMinX();
-			double boxMinY = bounds.getMinY();
-
 			// Check if boxes overlap
-			if (!(selMaxX < boxMinX || selMinX > boxMaxX || selMaxY < boxMinY || selMinY > boxMaxY)) {
+			if (key.touching(selection2)) {
 				overlapping.add(key.getName());
 			}
 		}
