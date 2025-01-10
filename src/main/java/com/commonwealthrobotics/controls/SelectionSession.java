7565a23ca3fe147f94d32e39641b5b8cef3b3b30
Kevin Harrington <kharrington@bancroftschool.org>
2025-01-10 09:28:03 -0500

    make the cruse not rotate by 90

--- src/main/java/com/commonwealthrobotics/controls/SelectionSession.java
+++ src/main/java/com/commonwealthrobotics/controls/SelectionSession.java
@@ -965,7 +965,7 @@ public class SelectionSession implements ICaDoodleStateUpdate {
 			indicator = CSG.unionAll(selectedCSG);
 		}
 		List<String> seleectedNames = selectedSnapshot();
-		TransformNR o = new TransformNR(RotationNR.getRotationY(0));
+		TransformNR o = new TransformNR(RotationNR.getRotationZ(90));
 
 		TransformNR g = o.times(wp.inverse());
 		Transform tf = TransformFactory.nrToCSG(g);
