1aab2c4fe0ec9ab2ccbcb968bf605cb6fdda85ff
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-21 15:03:14 -0400

    https://github.com/CommonWealthRobotics/CaDoodle-Application/issues/48
    Adding the correct timeline update timing

--- src/main/java/com/commonwealthrobotics/TimelineManager.java
+++ src/main/java/com/commonwealthrobotics/TimelineManager.java
@@ -26,11 +26,13 @@ public class TimelineManager {
 	private HBox timeline;
 	private ActiveProject ap;
 	private ArrayList<Button> buttons = new ArrayList<Button>();
+	private boolean updating = false;
 
 	public TimelineManager(ActiveProject activeProject) {
 		this.ap = activeProject;
 		ap.addListener(new ICaDoodleStateUpdate() {
-			boolean init=false;
+			boolean init = false;
+
 			@Override
 			public void onWorkplaneChange(TransformNR newWP) {
 				// TODO Auto-generated method stub
@@ -39,7 +41,7 @@ public class TimelineManager {
 
 			@Override
 			public void onUpdate(List<CSG> currentState, ICaDoodleOpperation source, CaDoodleFile file) {
-				if(init)
+				if (init)
 					update();
 			}
 
@@ -55,18 +57,19 @@ public class TimelineManager {
 
 			@Override
 			public void onRegenerateDone() {
+				clear();
 				update();
 			}
 
 			@Override
 			public void onInitializationStart() {
-				init=true;
+				init = true;
 			}
 
 			@Override
 			public void onInitializationDone() {
 				update();
-				init=false;
+				init = false;
 			}
 
 			@Override
@@ -83,18 +86,30 @@ public class TimelineManager {
 	}
 
 	public void update() {
-		int ci = ap.get().getCurrentIndex();
+		//new Exception().printStackTrace();
+		while (updating) {
+			try {
+				Thread.sleep(16);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Start Waiting for timeline to finish");
+			new Exception().printStackTrace();
+		}
+		updating = true;
 
 		BowlerStudio.runLater(() -> {
 			boolean addrem = false;
-			while (ci < buttons.size() && !ap.get().isForwardAvailible()) {
+			while ( ap.get().getCurrentIndex() < (buttons.size() - 1) && !ap.get().isForwardAvailible()) {
 				Button toRem = buttons.remove(buttons.size() - 1);
 				timeline.getChildren().remove(toRem);
 				addrem = true;
 			}
-			//clear();
+			// clear();
 			ArrayList<ICaDoodleOpperation> opperations = ap.get().getOpperations();
-			for (int i = buttons.size(); i < ci; i++) {
+			int s=opperations.size();
+			for (int i = buttons.size(); i < Math.max(s,  ap.get().getCurrentIndex()); i++) {
 				try {
 					ICaDoodleOpperation opp = opperations.get(i);
 					String text = (i + 1) + "\n" + opp.getType();
@@ -105,8 +120,8 @@ public class TimelineManager {
 							ap.get().moveToOpIndex(my);
 						}).start();
 					});
-					File f = ap.get().getTimelineImageFile(i);
-					//System.out.println("Laoding Image to timeline " + f.getAbsolutePath());
+					File f = ap.get().getTimelineImageFile(i - 1);
+					// System.out.println("Laoding Image to timeline " + f.getAbsolutePath());
 					Image image = new Image(f.toURI().toString());
 					ImageView value = new ImageView(image);
 					ImageView toolimage = new ImageView(image);
@@ -123,24 +138,48 @@ public class TimelineManager {
 					tooltip.setContentDisplay(ContentDisplay.TOP);
 					toAdd.setTooltip(tooltip);
 					buttons.add(toAdd);
+					// if (i != 0)
 					timeline.getChildren().add(toAdd);
 					addrem = true;
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
-			for (int i = 0; i < buttons.size(); i++)
-				buttons.get(i).setDisable(false);
-			buttons.get(ci - 1).setDisable(true);
+			if (buttons.size() > 0) {
+				for (int i = 0; i < buttons.size(); i++)
+					buttons.get(i).setDisable(false);
+				int index =  ap.get().getCurrentIndex() - 1;
+				buttons.get(index < 0 ? 0 : index).setDisable(true);
+			}
 			if (addrem)
-				Platform.runLater(() -> timelineScroll.setHvalue(1.0));
+				Platform.runLater(() -> {
+					timelineScroll.setHvalue(1.0);
+					updating = false;
+				});
+			else
+				updating = false;
 		});
-
+		while (updating) {
+			try {
+				Thread.sleep(16);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// System.out.println("End Waiting for timeline to finish");
+			// new Exception().printStackTrace();
+		}
 	}
 
 	public void clear() {
 		buttons.clear();
 		BowlerStudio.runLater(() -> timeline.getChildren().clear());
+		try {
+			Thread.sleep(16);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
