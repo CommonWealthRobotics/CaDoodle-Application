85a44602f70720387cd895733285bc64896eb52d
rondlh <77279634+rondlh@users.noreply.github.com>
2025-11-27 03:44:12 +0800

    Prevent opening Settings window several times
    Prevent opening Settings window several times

--- src/main/java/com/commonwealthrobotics/SettingsManager.java
+++ src/main/java/com/commonwealthrobotics/SettingsManager.java
@@ -428,6 +428,10 @@ public class SettingsManager implements ICSGClientEvent {
 
 	public static void launch(MainController mc) {
 		SettingsManager.mc = mc;
+		if (stage != null && stage.isShowing()) {
+			stage.toFront();
+			return;
+		}
 		try {
 			// Load the FXML file
 			
