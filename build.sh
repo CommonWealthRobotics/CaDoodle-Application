b4e0b250c12fe4a15750bde1ffcfaf146b967393
Kevin Harrington <mad.hephaestus@gmail.com>
2025-12-28 14:28:25 -0500

    update build scripts to the java 21

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export ARCH=x86_64
-JVM=zulu17.50.19-ca-fx-jdk17.0.11-linux_x64
+JVM=zulu21.46.19-ca-fx-jdk21.0.9-linux_x64
 set -e
 ZIP=$JVM.tar.gz
 export JAVA_HOME=$HOME/bin/java17/
