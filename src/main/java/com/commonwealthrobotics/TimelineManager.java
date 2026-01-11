66a52e9718016b292687442d5cf1494e3a67c8e7
Kevin Harrington <mad.hephaestus@gmail.com>
2026-01-11 15:38:30 -0500

    use the native image size for timeline

--- src/main/java/com/commonwealthrobotics/TimelineManager.java
+++ src/main/java/com/commonwealthrobotics/TimelineManager.java
@@ -117,7 +117,7 @@ public class TimelineManager {
 					ImageView img = (ImageView) b.getGraphic();
 					Image image = new Image(imageFile.toURI().toString());
 					BowlerStudio.runLater(() -> {
-						img.setImage(resizeImage(image, buttonSize, buttonSize));
+						img.setImage(image);
 					});
 					Log.debug("Updating " + imageFile);
 				}else
