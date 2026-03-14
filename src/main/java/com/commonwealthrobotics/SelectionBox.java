f49f131796e6743e61c70cc3d3583c24d066185f
rondlh <77279634+rondlh@users.noreply.github.com>
2026-03-14 18:47:41 +0800

    Fix a few extra spaces
    Fix a few extra spaces

--- src/main/java/com/commonwealthrobotics/SelectionBox.java
+++ src/main/java/com/commonwealthrobotics/SelectionBox.java
@@ -279,8 +279,8 @@ public class SelectionBox {
 		// Fast bounding box overlap check, quick fail if not intersecting
 		// bounding boxes
 		return ((selection2.getMaxX() > incoming.getMinX()) && (selection2.getMinX() < incoming.getMaxX())
-			&&  (selection2.getMaxY() > incoming.getMinY()) && (selection2.getMinY() < incoming.getMaxY())
-			&&  (selection2.getMaxZ() > incoming.getMinZ()) && (selection2.getMinZ() < incoming.getMaxZ()));
+			&& (selection2.getMaxY() > incoming.getMinY()) && (selection2.getMinY() < incoming.getMaxY())
+			&& (selection2.getMaxZ() > incoming.getMinZ()) && (selection2.getMinZ() < incoming.getMaxZ()));
 	}
 
 	public List<String> checkOverlap(CSG selection2) {
