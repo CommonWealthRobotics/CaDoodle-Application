5515dc8d0fe7b741a8df0bb907c5400a55cde97b
Kevin Harrington <mad.hephaestus@gmail.com>
2026-04-01 13:09:17 -0400

    catch the exception and prevent the NPE

--- src/main/java/com/commonwealthrobotics/SettingsManager.java
+++ src/main/java/com/commonwealthrobotics/SettingsManager.java
@@ -143,6 +143,8 @@ public class SettingsManager implements ICSGClientEvent {
 	@FXML
 	void onPinVersionSelect(ActionEvent event) {
 		String selectedItem = versionOptions.getSelectionModel().getSelectedItem();
+		if(selectedItem==null)
+			return;
 		Log.debug("onPinVersionSelect to " + selectedItem);
 		File f = new File(myVersionFileString);
 		try {
@@ -163,7 +165,7 @@ public class SettingsManager implements ICSGClientEvent {
 	void checkServerConfigs(KeyEvent event) {
 		try {
 			// Create a trust manager that ignores certificate errors
-			TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
 				public X509Certificate[] getAcceptedIssuers() {
 					return new X509Certificate[0];
 				}
@@ -173,7 +175,7 @@ public class SettingsManager implements ICSGClientEvent {
 
 				public void checkServerTrusted(X509Certificate[] certs, String authType) {
 				}
-			}};
+			} };
 
 			SSLContext sslContext = SSLContext.getInstance("TLS");
 			sslContext.init(null, trustAllCerts, null);
@@ -354,18 +356,18 @@ public class SettingsManager implements ICSGClientEvent {
 
 	private void setExplanationText(OperationResult result) {
 		switch (result) {
-			case INSERT :
-				insertionExplanation.setText(
-						"Insert will add this operation at the current position while keeping subsequent operations.");
-				break;
-			case PRUNE :
-				insertionExplanation.setText(
-						"Replace subsequent work with this change.\nThis will remove any work you've done after this point.");
-				break;
-			case ASK :
-				insertionExplanation
-						.setText("Always ask what I want to do with a popup window every time something is edited.");
-				break;
+		case INSERT:
+			insertionExplanation.setText(
+					"Insert will add this operation at the current position while keeping subsequent operations.");
+			break;
+		case PRUNE:
+			insertionExplanation.setText(
+					"Replace subsequent work with this change.\nThis will remove any work you've done after this point.");
+			break;
+		case ASK:
+			insertionExplanation
+					.setText("Always ask what I want to do with a popup window every time something is edited.");
+			break;
 		}
 		ConfigurationDatabase.put("CaDoodle", "Insertion Stratagy", result.name());
 		ConfigurationDatabase.save();
@@ -492,7 +494,11 @@ public class SettingsManager implements ICSGClientEvent {
 	}
 
 	private void updateVersionOptions() {
-		versionOptions.getItems().clear();
+		try {
+			versionOptions.getItems().clear();
+		} catch (Exception ex) {
+			Log.error(ex);
+		}
 		File[] listFiles = new File(bindir).listFiles();
 		if (listFiles != null) {
 			Arrays.sort(listFiles, (f1, f2) -> {
