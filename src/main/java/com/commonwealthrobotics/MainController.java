9f022f01822d0e5825a5e8cb3df622e0773ffd60
Kevin Harrington <mad.hephaestus@gmail.com>
2026-05-08 08:00:28 -0400

    embeddable BowlerStudio

--- src/main/java/com/commonwealthrobotics/MainController.java
+++ src/main/java/com/commonwealthrobotics/MainController.java
@@ -1110,6 +1110,7 @@ public class MainController implements ICaDoodleStateUpdate, ICameraChangeListen
 					s.setHeight(h - 1);
 					BowlerStudio.runLater(() -> s.setHeight(h));
 				});
+				BowlerStudio.go();
 			} catch (Exception e) {
 				com.neuronrobotics.sdk.common.Log.error(e);
 			}
