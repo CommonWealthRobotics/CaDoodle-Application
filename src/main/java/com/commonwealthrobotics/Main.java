b872e4eb754a96ba3e79fdeb468d386ebc0b1cc3
Kevin Harrington <mad.hephaestus@gmail.com>
2026-07-05 11:36:13 -0400

    bigger logfiles agaain

--- src/main/java/com/commonwealthrobotics/Main.java
+++ src/main/java/com/commonwealthrobotics/Main.java
@@ -174,8 +174,8 @@ public class Main extends Application {
 			logfile.delete();
 		try {
 			logfile.createNewFile();
-			// Log.enableDebugPrint(true);
-			Log.enableErrorPrint();
+			Log.enableDebugPrint(true);
+			//Log.enableErrorPrint();
 			Log.setFile(logfile);
 			com.neuronrobotics.sdk.common.Log.debug("Log file set to " + logfile.getAbsolutePath());
 
