05d53b6deeb7e33e3921f38efe13c0b03b69fc1e
Kevin Harrington <mad.hephaestus@gmail.com>
2026-07-29 13:02:55 -0400

    formatting

--- src/main/java/com/commonwealthrobotics/controls/SelectionSession.java
+++ src/main/java/com/commonwealthrobotics/controls/SelectionSession.java
@@ -872,19 +872,19 @@ public class SelectionSession implements ICaDoodleStateUpdate {
 	private void UpdateUIControls(List<CSG> cs) {
 		hideHalos();
 		resetSelectedCSGsFromCurrentState(cs);
-		
+
 		timeline.updateSelected(getSelected());
 		TickToc.tic("Start UpdateUIControls");
 		GridPane gp = new GridPane(5, 5);
-		
+
 		int line = 0;
 		parametrics.getChildren().clear();
 		parametrics.getChildren().add(gp);
 		int width = 170;
 		int c1Width = 100;
-		gp.setPrefWidth(width+c1Width);
+		gp.setPrefWidth(width + c1Width);
 		//gp.setMaxWidth(width+c1Width);
-		
+
 		ColumnConstraints col0 = new ColumnConstraints();
 		col0.setHgrow(Priority.NEVER);
 		col0.setMinWidth(c1Width);
