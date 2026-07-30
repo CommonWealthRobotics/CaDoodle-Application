15a1e44d7f383f98acbe17a566b81bff695eee4c
Kevin Harrington <mad.hephaestus@gmail.com>
2026-07-29 20:18:47 -0400

    dont loc the langauge it breaks on windows

--- src/main/java/com/commonwealthrobotics/ActiveProject.java
+++ src/main/java/com/commonwealthrobotics/ActiveProject.java
@@ -50,7 +50,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
