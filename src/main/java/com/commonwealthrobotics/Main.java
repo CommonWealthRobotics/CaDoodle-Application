fa075a43eec20f0462bebe19a8801df66a58e3a6
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-23 17:09:30 -0400

    fix log file use

--- src/main/java/com/commonwealthrobotics/Main.java
+++ src/main/java/com/commonwealthrobotics/Main.java
@@ -178,6 +178,8 @@ public class Main extends Application {
 			Log.setFile(logfile);
 			com.neuronrobotics.sdk.common.Log.debug("Log file set to " + logfile.getAbsolutePath());
 
+			Log.setFile(logfile);
+			com.neuronrobotics.sdk.common.Log.debug("Log file set to " + logfile.getAbsolutePath());
 			hand = new UncaughtExceptionHandler() {
 				@Override
 				public void uncaughtException(Thread t, Throwable e) {
