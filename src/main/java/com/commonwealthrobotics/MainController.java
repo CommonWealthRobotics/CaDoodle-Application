fe920dfc0ce5ca71265b0c74ee5223a361c19c8d
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-26 16:48:29 -0400

    close https://github.com/CommonWealthRobotics/CaDoodle-Application/issues/307

--- src/main/java/com/commonwealthrobotics/MainController.java
+++ src/main/java/com/commonwealthrobotics/MainController.java
@@ -1424,9 +1424,6 @@ public class MainController implements ICaDoodleStateUpdate, ICameraChangeListen
 				boolean shiftDown = me.isShiftDown();
 				boolean primaryButtonDown = me.isPrimaryButtonDown();
 				boolean secondaryButtonDown = me.isSecondaryButtonDown();
-				boolean middle = me.isMiddleButtonDown();
-				if (middle)
-					return true;
 				boolean ctrl = me.isControlDown();
 				if (ctrl && primaryButtonDown && (!shiftDown))
 					return true;
@@ -1443,7 +1440,7 @@ public class MainController implements ICaDoodleStateUpdate, ICameraChangeListen
 				boolean middle = me.isMiddleButtonDown();
 				boolean ctrl = me.isControlDown();
 				if (middle)
-					return false;
+					return true;
 				if ((shiftDown) && secondaryButtonDown)
 					return true;
 				if (ctrl && shiftDown && primaryButtonDown)
