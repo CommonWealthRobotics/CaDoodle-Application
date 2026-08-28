e2918f43c40b15c94e5877013b75f1e15d4d39bd
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-28 10:50:17 -0400

    updated view cube

--- src/main/java/com/commonwealthrobotics/ViewCube.java
+++ src/main/java/com/commonwealthrobotics/ViewCube.java
@@ -5,6 +5,8 @@ import javafx.scene.transform.Affine;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.PickResult;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.geometry.Point3D;
 import org.fxyz3d.shapes.primitives.CuboidMesh;
 import org.fxyz3d.shapes.primitives.TexturedMesh;
@@ -14,34 +16,25 @@ import com.neuronrobotics.bowlerstudio.threed.BowlerStudio3dEngine;
 import com.neuronrobotics.bowlerstudio.threed.IControlsMap;
 import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR;
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import com.neuronrobotics.sdk.common.Log;
+
+import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.Cube;
 
 public class ViewCube {
-	private static TexturedMesh meshView;
-	private static  BowlerStudio3dEngine engine;
-	private static  boolean focusTrig = false;
-	private  static float cubeSize = 100f;
+	private static BowlerStudio3dEngine engine;
+	private static boolean focusTrig = false;
+	private static float cubeSize = 100f;
+	private static PhongMaterial phongMaterialCube = new PhongMaterial(Color.YELLOW);
+	private static PhongMaterial phongMaterialEdge = new PhongMaterial(Color.ORANGE);
+	private static PhongMaterial phongMaterialCorner = new PhongMaterial(Color.WHITE);
 
 	public static void createTexturedCube(BowlerStudio3dEngine e) {
 
 		engine = e;
-		Affine rot = TransformFactory.nrToAffine(new TransformNR(new RotationNR(-90, 0, 0)));
-		meshView = new CuboidMesh(cubeSize, cubeSize, cubeSize);
-		meshView.setTextureModeImage(MainController.class.getResource("navCube.png").toExternalForm());
-		meshView.getTransforms().add(rot);
+		addTexturedCube();
 
-		meshView.setOnMousePressed(event -> {
-			focusTrig = true;
-		});
-
-		meshView.setOnMouseDragged(event -> {
-			engine.focusOrientation(null, null, 0); // Send cancel
-			focusTrig = false;
-		});
-
-		meshView.setOnMouseReleased(event -> {
-			if (focusTrig)
-				handleMouseClick(event);
-		});
+		addSurface(new Cube(2, cubeSize, cubeSize).toCSG(), new TransformNR(cubeSize/2, 0, 0), phongMaterialCube,"Front");
 
 		engine.setControlsMap(new IControlsMap() {
 
@@ -76,11 +69,69 @@ public class ViewCube {
 
 		// meshView.setOnMouseClicked(this::handleMouseClick);
 		// meshView.setOnMouseClicked(event -> handleMouseClick(event, meshView));
-		engine.addUserNode(meshView);
-		return ;
+
+		return;
 	}
 
-	private static void handleMouseClick(MouseEvent event) {
+	private static void addSurface(CSG csg, TransformNR transformNR, PhongMaterial phongMaterialCube2, String string) {
+		String translation = ActiveProject.getTranslation(string);
+		CSG placed = csg.transformed(TransformFactory.nrToCSG(transformNR));
+		MeshView meshView = placed.newMesh();
+		meshView.setMaterial(phongMaterialCube2);
+
+		meshView.setOnMousePressed(event -> {
+			focusTrig = true;
+		});
+
+		meshView.setOnMouseDragged(event -> {
+			engine.focusOrientation(null, null, 0); // Send cancel
+			focusTrig = false;
+		});
+
+		meshView.setOnMouseReleased(event -> {
+			if (focusTrig) {
+				if (event.getPickResult().getIntersectedNode() == meshView) {
+					double az = 0;
+					if (Math.abs(transformNR.getX()) > 0.1 || Math.abs(transformNR.getY()) > 0.1)
+						az=Math.toDegrees(Math.atan2(transformNR.getY(), transformNR.getX()));
+					double el =0;
+					if (Math.abs(transformNR.getZ()) > 0.1 ) {
+						TransformNR alligned = transformNR.times(new TransformNR(new RotationNR(0, -az, 0)));
+						el=Math.toDegrees(Math.atan2(alligned.getX(), alligned.getZ()));
+					}
+					TransformNR target= new TransformNR(0, 0, 0, new RotationNR(0, az, el));
+					Log.debug("Targeting "+target.toSimpleString());
+					engine.focusOrientation(target);
+				}
+			}
+		});
+
+		engine.addUserNode(meshView);
+	}
+
+	private static void addTexturedCube() {
+		Affine rot = TransformFactory.nrToAffine(new TransformNR(new RotationNR(-90, 0, 0)));
+		TexturedMesh meshView = new CuboidMesh(cubeSize, cubeSize, cubeSize);
+		meshView.setTextureModeImage(MainController.class.getResource("navCube.png").toExternalForm());
+		meshView.getTransforms().add(rot);
+
+		meshView.setOnMousePressed(event -> {
+			focusTrig = true;
+		});
+
+		meshView.setOnMouseDragged(event -> {
+			engine.focusOrientation(null, null, 0); // Send cancel
+			focusTrig = false;
+		});
+
+		meshView.setOnMouseReleased(event -> {
+			if (focusTrig)
+				handleMouseClick(event, meshView);
+		});
+		// engine.addUserNode(meshView);
+	}
+
+	private static void handleMouseClick(MouseEvent event, TexturedMesh meshView) {
 		PickResult pickResult = event.getPickResult();
 
 		if (pickResult.getIntersectedNode() == meshView) {
